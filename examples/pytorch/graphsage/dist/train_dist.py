322dc52dd91226ca0e945539955a0289ea476c4f
zhitingz <zhitingz@cs.utexas.edu>
2023-05-16 03:30:03 -0400

    allow both option
    Signed-off-by: zhitingz <zhitingz@cs.utexas.edu>

--- examples/pytorch/graphsage/dist/train_dist.py
+++ examples/pytorch/graphsage/dist/train_dist.py
@@ -459,6 +459,9 @@ if __name__ == "__main__":
         "--local-rank", type=int, help="get rank of the process"
     )
     parser.add_argument(
+        "--local_rank", type=int, help="get rank of the process"
+    )
+    parser.add_argument(
         "--standalone", action="store_true", help="run in the standalone mode"
     )
     parser.add_argument(
